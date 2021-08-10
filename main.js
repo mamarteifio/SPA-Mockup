fa6328c50377a345c855ca5e497ce92b296a9d22
Glitch (glitch-hello-website) <none>
2021-08-10 02:05:03 +0000

    👲📯 Checkpoint ./style.css:1621703/2800 ./index.html:1621703/3567 ./main.js:1621703/632

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 console.log("main.js is active");
 
+var navElements = {
+  navUser: document.getElementById("navUser"),
+  navAlerts: document.getElementById("navAlerts"),
+  navSettings: document.getElementById("navSettings"),
+}
